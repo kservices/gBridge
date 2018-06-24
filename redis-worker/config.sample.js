6484402c2e02dd41810a33fedf187fc05abd5a38
Peter Kappelt <kappelt.peter@gmail.com>
2018-06-24 16:33:31 +0200

    Updated sample config

--- redis-worker/config.sample.js
+++ redis-worker/config.sample.js
@@ -5,7 +5,10 @@ var config = {
     
     //Connection parameters for MQTT server
     //See https://www.npmjs.com/package/mqtt for parameters
-    "mqtt": "mqtt://mqtt1.int.kappelt.net:1883"
+    "mqtt": "mqtt://mqtt1.int.kappelt.net:1883",
+
+    //generate an API-Key in Google Cloud Dashboard for your project. Make sure that the "Homegraph API" is enabled!
+    "homegraph-api-key": "abc123"
 };
 
 module.exports = config;
