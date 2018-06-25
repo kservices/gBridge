13d11a4faa9ae902efe097adf5e78e3d5ba2ee1e
Peter Kappelt <kappelt.peter@gmail.com>
2018-06-25 14:33:47 +0200

    Simplified user interface

--- manage/resources/views/layout/loggedin.blade.php
+++ manage/resources/views/layout/loggedin.blade.php
@@ -19,24 +19,16 @@
 				<a href="{{ url('/') }}" class="brand-logo" style="padding: 7px 20px 7px 20px;">gBridge</a>
 				<a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
 				<ul id="nav-mobile" class="right hide-on-med-and-down">
-					<li class="{{ (Request::is('device*') ||  Request::is('accesskey*')) ? 'active':'' }}"><a href="{{ route('device.index') }}">Management</a></li>
+					<li class="{{ Request::is('device*') ? 'active':'' }}"><a href="{{ route('device.index') }}">Devices</a></li>
+					<li class="{{ Request::is('accesskey*') ? 'active':'' }}"><a href="{{ route('accesskey.index') }}">Accesskeys</a></li>
 					<li class="{{ Request::is('profile*') ? 'active':'' }}"><a class="dropdown-trigger" href="#" data-target="user-dropdown">Hi {{ Auth::user()->email }}!<i class="material-icons right">arrow_drop_down</i></a></li>
 				</ul>
 			</div>
-
-			{{-- those tabs are only shown for management-sites --}}
-			@if( Request::is('device*') ||  Request::is('accesskey*'))
-			<div class="nav-content">
-				<ul class="tabs tabs-transparent">
-					<li class="tab"><a class="{{ Request::is('device*') ? 'active' : '' }}" href="{{ route('device.index') }}">Devices</a></li>
-					<li class="tab"><a class="{{ Request::is('accesskey*') ? 'active' : '' }}" href="{{ route('accesskey.index') }}">Accesskeys</a></li>
-				</ul>
-			</div>
-			@endif
 			
 			{{-- mobile menu --}}
 			<ul class="sidenav" id="mobile-demo">
-				<li class="{{ (Request::is('device*') ||  Request::is('accesskey*')) ? 'active':'' }}"><a href="{{ route('device.index') }}">Management</a></li>
+				<li class="{{ Request::is('device*') ? 'active':'' }}"><a href="{{ route('device.index') }}">Devices</a></li>
+				<li class="{{ Request::is('accesskey*') ? 'active':'' }}"><a href="{{ route('accesskey.index') }}">Accesskeys</a></li>
 				<li class="{{ Request::is('profile*') ? 'active':'' }}"><a href="{{ route('profile.index') }}">My Account</a></li>
 				<li><a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a></li>
 			</ul>
