d4cea4f968c9dfdfb88fe4fa0a0a13c4f7ae1dd1
Peter Kappelt <kappelt.peter@gmail.com>
2018-06-28 22:32:16 +0200

    Using environment variables in config

--- redis-worker/config.sample.js
+++ redis-worker/config.sample.js
@@ -1,14 +1,16 @@
+//You can either set environment variables to configure this module, then just copy config.sample.js to config.js
+//Otherwise, copy this file to config.js and change the values there
 var config = {
     //Connection parameters for the Redis server
     //See https://www.npmjs.com/package/redis for documentation of connection options
-    "redis": "redis://redis1.int.kappelt.net:6379",
+    "redis": process.env.GBRIDGE_REDISWORKER_REDIS || "redis://redis1.int.kappelt.net:6379",
     
     //Connection parameters for MQTT server
     //See https://www.npmjs.com/package/mqtt for parameters
-    "mqtt": "mqtt://mqtt1.int.kappelt.net:1883",
+    "mqtt": process.env.GBRIDGE_REDISWORKER_MQTT || "mqtt://mqtt1.int.kappelt.net:1883",
 
     //generate an API-Key in Google Cloud Dashboard for your project. Make sure that the "Homegraph API" is enabled!
-    "homegraph-api-key": "abc123"
+    "homegraph-api-key": process.env.GBRIDGE_REDISWORKER_HOMEGRAPHKEY || "your-key"
 };
 
 module.exports = config;
