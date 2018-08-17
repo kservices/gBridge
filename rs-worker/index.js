7f606499af8c8c362d7ab5e8ead316a142be2c84
Peter Kappelt <kappelt.peter@gmail.com>
2018-08-17 15:56:03 +0200

    Report State now reporting new power states of device

--- rs-worker/index.js
+++ rs-worker/index.js
@@ -25,9 +25,9 @@ var mqtt = mqttlib.connect(config.mqtt, {
 
 var accesstoken = "";
 
-function newAccessToken(){
+function newAccessToken() {
     var claims = {};
-    
+
     //issued at, expiry time and issuer will be handled by jwt lib
 
     claims["scope"] = "https://www.googleapis.com/auth/homegraph";
@@ -39,23 +39,23 @@ function newAccessToken(){
         issuer: serviceAcc.client_email
     });
 
-    axios.post('https://accounts.google.com/o/oauth2/token', 
+    axios.post('https://accounts.google.com/o/oauth2/token',
         'grant_type=urn:ietf:params:oauth:grant-type:jwt-bearer&assertion=' + token, {
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Authorization': 'Bearer ' + token
-        }
-    }).then(function (response){
-        if(!('access_token' in response.data)){
-            console.error("[NewAccessToken] Key \"access_token\" was not in response data");
-            return;
-        }
-        accesstoken = response.data.access_token;
-        console.log("[NewAccessToken] Successfully generated new access token!");
-        //console.log(accesstoken);
-    }).catch(function (error){
-        console.log("[NewAccessToken] error: " + error);
-    });
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': 'Bearer ' + token
+            }
+        }).then(function (response) {
+            if (!('access_token' in response.data)) {
+                console.error("[NewAccessToken] Key \"access_token\" was not in response data");
+                return;
+            }
+            accesstoken = response.data.access_token;
+            console.log("[NewAccessToken] Successfully generated new access token!");
+            //console.log(accesstoken);
+        }).catch(function (error) {
+            console.log("[NewAccessToken] error: " + error);
+        });
 }
 
 //Periodically create a new access token, after (less than) half of expiry period
@@ -65,83 +65,87 @@ setInterval(newAccessToken, (config.accesskey_renew_interval - 2) * 500);
 /**
  * Report state
  */
-function reportState(userid, requestid){
-    redis.get(`gbridge:u${userid}:devices`, function(err, response){
-        if(err){
-            console.error("Redis client error while fetching device for user " + userid +": " + err);
+function reportState(userid, requestid) {
+    redis.get(`gbridge:u${userid}:devices`, function (err, response) {
+        if (err) {
+            console.error("Redis client error while fetching device for user " + userid + ": " + err);
             return;
         }
-        if(response == null){
+        if (response == null) {
             console.error("Redis client error while fetching devices for user " + userid + ": device list empty");
             return;
         }
-       
+
         response = JSON.parse(response);
 
         rsData = {
             'agent_user_id': userid,
-            'payload': {'devices': {'states': {}}}
+            'payload': { 'devices': { 'states': {} } }
         };
 
-        if(requestid != null){
+        if (requestid != null) {
             rsData['requestId'] = requestid;
         }
 
-        Promise.all(Object.keys(response).map(function(deviceid){
+        Promise.all(Object.keys(response).map(function (deviceid) {
             let traits = response[deviceid];
 
-            return new Promise(function(resolve, reject){
-                redis.hgetall(`gbridge:u${userid}:d${deviceid}`, function(err, data){
+            return new Promise(function (resolve, reject) {
+                redis.hgetall(`gbridge:u${userid}:d${deviceid}`, function (err, data) {
                     rsData['payload']['devices']['states'][deviceid] = {};
 
-                    if(err || (!data)){
+                    if (err || (!data)) {
                         //default values if not yet set, do not throw error
-                        if(traits.includes('OnOff')){
+                        if (traits.includes('OnOff')) {
                             rsData['payload']['devices']['states'][deviceid]['on'] = false;
                         }
-                        if(traits.includes('Brightness')){
+                        if (traits.includes('Brightness')) {
                             rsData['payload']['devices']['states'][deviceid]['brightness'] = 0;
                         }
                         //return reject(err);
                         resolve();
                         return;
                     }
-                    
-                    if(data){
-                        if(traits.includes('OnOff')){
-                            if('onoff' in data){
-                                rsData['payload']['devices']['states'][deviceid]['on'] = (data['onoff'] != '0') ? true:false;
-                            }else{
+
+                    if (data) {
+                        if (traits.includes('OnOff')) {
+                            if ('onoff' in data) {
+                                rsData['payload']['devices']['states'][deviceid]['on'] = (data['onoff'] != '0') ? true : false;
+                            } else {
                                 rsData['payload']['devices']['states'][deviceid]['on'] = false;
                             }
                         }
-                        if(traits.includes('Brightness')){
-                            if('brightness' in data){
+                        if (traits.includes('Brightness')) {
+                            if ('brightness' in data) {
                                 rsData['payload']['devices']['states'][deviceid]['brightness'] = parseInt(data['brightness']);
-                            }else{
+                            } else {
                                 rsData['payload']['devices']['states'][deviceid]['brightness'] = 0;
                             }
+                        }
+
+                        if ('power' in data) {
+                            rsData['payload']['devices']['states'][deviceid]['online'] = (data['power'] != '0') ? true : false;
                         }
                     }
                     resolve();
                 });
             });
-        })).then(function(){
+        })).then(function () {
             //console.log(JSON.stringify(rsData));
-            axios.post('https://homegraph.googleapis.com/v1/devices:reportStateAndNotification', 
+            axios.post('https://homegraph.googleapis.com/v1/devices:reportStateAndNotification',
                 rsData, {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Bearer ' + accesstoken,
-                    'X-GFE-SSL': 'yes'
-                }
-            }).then(function (response){
-                //console.log('[Report State]  Response: ' + console.log(util.inspect(response, false, null)));
-                //Response was OK :)
-            }).catch(function (error){
-                console.log("[Report State] HTTP error: " + error);
-            });
-        }).catch(function(err){
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': 'Bearer ' + accesstoken,
+                        'X-GFE-SSL': 'yes'
+                    }
+                }).then(function (response) {
+                    //console.log('[Report State]  Response: ' + console.log(util.inspect(response, false, null)));
+                    //Response was OK :)
+                }).catch(function (error) {
+                    console.log("[Report State] HTTP error: " + error);
+                });
+        }).catch(function (err) {
             console.log("Error while fetching report-state trait data: " + err);
         });
 
@@ -151,13 +155,13 @@ function reportState(userid, requestid){
 /**
  * Redis client functions
  */
-redis_subscribe.on('connect', function() {
+redis_subscribe.on('connect', function () {
     console.log('Redis client (subscribe) connected');
 });
 redis_subscribe.on('error', function (err) {
     console.log('Redis client (subscribe) error: ' + err);
 });
-redis.on('connect', function() {
+redis.on('connect', function () {
     console.log('Redis client connected');
 });
 redis.on('error', function (err) {
@@ -166,67 +170,67 @@ redis.on('error', function (err) {
 
 redis_subscribe.on("pmessage", function (pattern, channel, message) {
     //only proactively notify on sync
-    if(message != 'SYNC'){
+    if (message != 'SYNC') {
         return;
     }
 
     var channelMatch = /(?:gbridge:u)(\d+)(?::d)(\d+)(?::grequest)/;
 
-    if(!channelMatch.test(channel)){
+    if (!channelMatch.test(channel)) {
         console.log(`Redis client (subscribe) error (test): Received data on malformed channel "${channel}" for pattern "${pattern}" with data "${message}"`);
         return;
     }
 
     var matches = channelMatch.exec(channel);
-    if((matches == null) || (matches.length < 3)){      //3 variables are expected in the match: The whole match itself and the two capture groups
+    if ((matches == null) || (matches.length < 3)) {      //3 variables are expected in the match: The whole match itself and the two capture groups
         console.log(`Redis client (subscribe) error (match): Received data on malformed channel "${channel}" for pattern "${pattern}" with data "${message}"`);
         return;
     }
 
     var userid = matches[1];
-    redis.hget(`gbridge:u${userid}:d0`, 'grequestid', function(err, response){
-        if(err){
+    redis.hget(`gbridge:u${userid}:d0`, 'grequestid', function (err, response) {
+        if (err) {
             console.error("Redis client error while fetching request id: " + err);
             return;
         }
-        if(response == null){
+        if (response == null) {
             console.error("Redis client error while fetching request id: undefined request id");
             return;
         }
         reportState(userid, response);
     });
 
-    
+
 });
 redis_subscribe.psubscribe("gbridge:u*:d*:grequest");
 
 /**
  * MQTT-Client functions
  */
-mqtt.on('connect', function(){
+mqtt.on('connect', function () {
     console.log("MQTT client connected");
 });
-mqtt.on('error', function(error){
+mqtt.on('error', function (error) {
     console.log("MQTT client error: " + error);
 });
-mqtt.on('offline', function() {
+mqtt.on('offline', function () {
     console.log("MQTT client offline!");
 });
-mqtt.on('reconnect', function() {
+mqtt.on('reconnect', function () {
     console.log("MQTT client reconnected!");
 });
 
-mqtt.on('message', function(topic, message){
+mqtt.on('message', function (topic, message) {
     message = message.toString();
     var topicMatch = /^(?:gBridge\/u)(\d+)(?:\/d)(\d+)(?:\/)([^\/]+)/;
 
-    if(!topicMatch.test(topic)){
+    if (!topicMatch.test(topic)) {
         console.log(`MQTT client error (test): Received malformed topic "${topic}" with message "${message}"`);
         return;
     }
 
     var matches = topicMatch.exec(topic);
-    if((matches == null) || (matches.length < 4)){      //4 variables are expected in the match: The whole match itself and the three capture groups
+    if ((matches == null) || (matches.length < 4)) {      //4 variables are expected in the match: The whole match itself and the three capture groups
         console.log(`MQTT client error (match): Received malformed topic "${topic}" with message "${message}"`);
         return;
     }
@@ -235,65 +239,65 @@ mqtt.on('message', function(topic, message){
     var deviceid = matches[2];
     var devicetrait = matches[3].toLowerCase();
 
-    if(devicetrait === "onoff"){
-        if((message == "0") || (message == "false") || (message == "off")){
+    if (devicetrait === "onoff") {
+        if ((message == "0") || (message == "false") || (message == "off")) {
             message = 0;
-        }else{
+        } else {
             message = 1;
         }
-    }else if(devicetrait === "brightness"){
+    } else if (devicetrait === "brightness") {
         var brightness = Number.parseInt(message);
-        if(Number.isNaN(brightness)){
+        if (Number.isNaN(brightness)) {
             console.log(`MQTT client error: Wrong brightness "${message}" for user ${userid}`);
             return;
         }
-        if(brightness < 0){
+        if (brightness < 0) {
             brightness = 0;
         }
-        if(brightness > 100){
+        if (brightness > 100) {
             brightness = 100;
         }
 
         message = brightness;
-    }else if(devicetrait === "power"){
+    } else if (devicetrait === "power") {
         //device reporting power state
-        if((message == "0") || (message == "false") || (message == "off")){
+        if ((message == "0") || (message == "false") || (message == "off")) {
             message = 0;
-        }else{
+        } else {
             message = 1;
         }
-    }else{
+    } else {
         console.log(`MQTT client error: Unsupported trait "${devicetrait}" for user ${userid}`);
         return;
     }
 
     redis.hset(`gbridge:u${userid}:d${deviceid}`, devicetrait, message);
 
-    redis.hget(`gbridge:u${userid}:d0`, 'grequesttype', function(err, response){
-        if(err){
+    redis.hget(`gbridge:u${userid}:d0`, 'grequesttype', function (err, response) {
+        if (err) {
             console.error("Redis client error while fetching last request type: " + err);
             return;
         }
-        if(response == null){
+        if (response == null) {
             console.error("Redis client error while fetching last request type: undefined request type");
             return;
         }
 
-        if(response == 'EXECUTE'){
+        if (response == 'EXECUTE') {
             //get last requestid if if the previous request was EXECUTE
-            redis.hget(`gbridge:u${userid}:d0`, 'grequestid', function(err, response){
-                if(err){
+            redis.hget(`gbridge:u${userid}:d0`, 'grequestid', function (err, response) {
+                if (err) {
                     console.error("Redis client error while fetching request id: " + err);
                     return;
                 }
-                if(response == null){
+                if (response == null) {
                     console.error("Redis client error while fetching request id: undefined request id");
                     return;
                 }
                 //console.log("Good set");
                 reportState(userid, response);
             });
-        }else{
+        } else {
             //console.log("Bad set");
             reportState(userid, null);
         }
@@ -302,3 +306,5 @@ mqtt.on('message', function(topic, message){
 });
 //MQTT topic format is here gBridge/u<userid>/d<deviceid>/<trait>/set
 mqtt.subscribe('gBridge/+/+/+/set');
+//Subscribe to power topic, too
+mqtt.subscribe("gBridge/+/+/power");
