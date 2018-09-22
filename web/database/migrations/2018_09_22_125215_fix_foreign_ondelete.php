a53e5422c7290cefa859b2744a1db9d1236141da
Peter Kappelt <kappelt.peter@gmail.com>
2018-09-22 15:01:40 +0200

    Fixed foreign key constraints (forgot onDelete-Statement)

---
+++ web/database/migrations/2018_09_22_125215_fix_foreign_ondelete.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class FixForeignOndelete extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('device', function (Blueprint $table) {
+            $table->dropForeign('device_user_id_foreign');
+            $table->foreign('user_id')->references('user_id')->on('user')->onDelete('cascade');
+        });
+        Schema::table('google_accesskey', function (Blueprint $table) {
+            $table->dropForeign('google_accesskey_user_id_foreign');
+            $table->foreign('user_id')->references('user_id')->on('user')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('device', function (Blueprint $table) {
+            $table->dropForeign('device_user_id_foreign');
+            $table->foreign('user_id')->references('user_id')->on('user');
+        });
+        Schema::table('google_accesskey', function (Blueprint $table) {
+            $table->dropForeign('google_accesskey_user_id_foreign');
+            $table->foreign('user_id')->references('user_id')->on('user');
+        });
+    }
+}
