3dd7a6ae63f0c8ad9ae01f3f4fb59522fd54b802
Gert-Jan <gertjancom@gmail.com>
2018-11-21 17:18:26 +0100

    Add CRUD api endpoints for device and get endpoints for device-types and trait-types

--- web/app/Providers/AppServiceProvider.php
+++ web/app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Services\DeviceService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind('App\Services\DeviceService', function () {
+            return new DeviceService();
+        });
     }
 }
