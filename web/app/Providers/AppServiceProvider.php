6c3aded2b451aff203ece4f74f4d568cd6c4c00f
Peter Kappelt <kappelt.peter@gmail.com>
2018-12-09 16:13:25 +0100

    Added user specified URL for reverse proxy setup

--- web/app/Providers/AppServiceProvider.php
+++ web/app/Providers/AppServiceProvider.php
@@ -16,6 +16,16 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        //Possibly allow to force an URL, sometimes necessary if behind a reverse proxy
+        $proxy_url    = getenv('PROXY_URL');
+        $proxy_schema = getenv('PROXY_SCHEMA');
+        if (!empty($proxy_url)) {
+            URL::forceRootUrl($proxy_url);
+        }
+        if (!empty($proxy_schema)) {
+            URL::forceSchema($proxy_schema);
+        }
     }
 
     /**
