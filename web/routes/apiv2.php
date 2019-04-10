8f6ac3586b1e45370f47a1c52d5947ab973f1d4a
Gert-Jan <gertjancom@gmail.com>
2019-04-10 20:50:13 +0200

    Add deletion of device to the API v2 Routes

--- web/routes/apiv2.php
+++ web/routes/apiv2.php
@@ -24,6 +24,7 @@ Route::get('device', 'ApiV2\ApiV2@getDevices');
 Route::post('device', 'ApiV2\ApiV2@createDevice');
 Route::get('device/{device}', 'ApiV2\ApiV2@getDeviceById');
 Route::patch('device/{device}', 'ApiV2\ApiV2@updateDeviceById');
+Route::delete('device/{device}', 'ApiV2\ApiV2@deleteDeviceById');
 
 Route::get('user', 'ApiV2\ApiV2@getUserDetails');
 Route::post('user/password', 'ApiV2\ApiV2@updateUserPassword');
