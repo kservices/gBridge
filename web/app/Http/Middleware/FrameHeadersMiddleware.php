4ca92b368761d117ac379d36873552ad0d83c33b
Peter Kappelt <kappelt.peter@gmail.com>
2019-05-14 10:50:54 +0200

    Security improvements

---
+++ web/app/Http/Middleware/FrameHeadersMiddleware.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class FrameHeadersMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+        $response->header('X-Frame-Options', 'DENY');
+        return $response;
+    }
+}
