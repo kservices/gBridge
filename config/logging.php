f8f3ce8256130ade707272db102cc7be90d987b4
Princewill Ochui <ochuiprincewill411@gmail.com>
2022-10-03 21:05:33 +0100

    sentry

--- config/logging.php
+++ config/logging.php
@@ -117,6 +117,10 @@ return [
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
         ],
+        
+        'sentry' => [
+            'driver' => 'sentry',
+        ],
     ],
 
 ];
